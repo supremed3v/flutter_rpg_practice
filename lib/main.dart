7ee5a2b088df05fc2fd04bfb70913f49f25c375b
Saad Siddiqui <saadsiddiqui-programmer@protonmail.com>
2024-05-13 15:49:25 -0700

    working on homescreen

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rpg/screens/home/home.dart';
+import 'package:flutter_rpg/theme.dart';
+
+void main() {
+  runApp(MaterialApp(theme: primaryTheme, home: const Home()));
+}
+
+class SandBox extends StatelessWidget {
+  const SandBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sandbox'),
+        backgroundColor: Colors.grey,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: const Text("Sandbox"),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home/home.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rpg/shared/styled_button.dart';
+import 'package:flutter_rpg/shared/styled_text.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const StyledTitle('Your Characters'),
+          centerTitle: true,
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              const StyledText("Character List"),
+              const StyledHeading("Character List"),
+              const StyledTitle("Character List"),
+              StyledButton(
+                  onPressed: () {}, child: const StyledHeading("Create New"))
+            ],
+          ),
+        ));
+  }
+}
