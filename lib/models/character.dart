10ebb94eb207baacff79952ca32b810c473cf173
Saad Siddiqui <saadsiddiqui-programmer@protonmail.com>
2024-05-15 02:00:02 +0500

    completed models

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_rpg/models/skill.dart';
 import 'package:flutter_rpg/models/stats.dart';
 import 'package:flutter_rpg/models/vocation.dart';
 
@@ -11,6 +12,7 @@ class Character with Stats {
   });
 
 // Fields
+  final Set<Skill> skills = {};
   final Vocation vocation;
   final String name;
   final String slogan;
@@ -22,5 +24,10 @@ class Character with Stats {
 
   void toggleIsFav() {
     _isFav = !_isFav;
+  }
+
+  void updateSkill(Skill skill) {
+    skills.clear();
+    skills.add(skill);
   }
 }
